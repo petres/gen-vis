af22a1583b72071939ee9d462e1d87ef716570e8
petres <peter.reschenhofer@gmail.com>
2022-10-14 16:50:02 +0200

    only attach once

--- src/lib.js
+++ src/lib.js
@@ -12,7 +12,7 @@ window.mountGenVisElement = element => {
             return {...acc, [name.substring(5)]: element.getAttribute(name)};
         }, {});
     // console.log(attrs);
-    const app = createApp(App, attrs)
+    createApp(App, attrs)
         .use(createPinia())
         .mount(element)
 }
@@ -20,6 +20,9 @@ window.mountGenVisElement = element => {
 window.mountGenVisByClass = cl => {
     const es = document.getElementsByClassName(cl);
     for (const e of es) {
-        window.mountGenVisElement(e);
+        if (!e.classList.contains('gen-vis-attached')) {
+            e.classList.add('gen-vis-attached')
+            window.mountGenVisElement(e);
+        }
     }
 }
